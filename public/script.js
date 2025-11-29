141b7d3ba42aa86391403426053828291a8c9f0f
Juan David <davidtoloza1234@hotmail.com>
2025-11-29 17:06:48 -0500

    three comit

--- public/script.js
+++ public/script.js
@@ -1,4 +1,17 @@
-document.getElementById("btnChangePositionSvgLogin").addEventListener("click", ()=>{
+
+// funcion capyurar items 
+function capItem(name) {
+    let item = document.getElementById(name);
+    return item.value;
+}
+const btnChangePositionSvgLogin = document.getElementById("btnChangePositionSvgLogin");
+const btnChangePositionSvgRegister = document.getElementById("btnChangePositionSvgRegister");
+
+btnChangePositionSvgLogin.addEventListener("click", ()=>{
     document.getElementById("image-svg-slice").classList.add("image-svg-slice-right")
-    console.log("hola")
+    document.getElementById("image-svg-slice").classList.remove("image-svg-slice-left")
+})
+btnChangePositionSvgRegister.addEventListener("click", ()=>{
+    document.getElementById("image-svg-slice").classList.add("image-svg-slice-left")
+    document.getElementById("image-svg-slice").classList.remove("image-svg-slice-right")
 })
