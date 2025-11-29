0ab9a60c23c10a0e1d33e2d39bc71889bc46c718
Juan David <davidtoloza1234@hotmail.com>
2025-11-29 16:50:46 -0500

    three comit

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnChangePositionSvgLogin").addEventListener("click", ()=>{
+    document.getElementById("image-svg-slice").classList.add("image-svg-slice-right")
+    console.log("hola")
+})
